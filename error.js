761dbaa327e8eb0328e59de220e70276b58b5af9
biswarup <bis.banerjee.bb@gmail.com>
2017-10-26 17:05:12 +0530

    more edits

--- error.js
+++ error.js
@@ -1,6 +1,6 @@
 	$(document).ready(function() {
  		$("#image404").toggleClass('flip');
  		$("#error-heading").toggleClass('flip-again');
- 		$("#error-heading").toggleClass('flap');
+ 		$("#page_not_found_image").toggleClass('flap');
 
 	});
