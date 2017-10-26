893cd6a6cde59a435ff3e5c89cb7d02cbdcd68a4
biswarup <bis.banerjee.bb@gmail.com>
2017-10-26 18:41:10 +0530

    added csrf error page

--- error.js
+++ error.js
@@ -2,5 +2,7 @@
  		$("#image404").toggleClass('flip');
  		$("#error-heading").toggleClass('flip-again');
  		$("#page_not_found_image").toggleClass('flap');
+ 		$("#csrf_error").toggleClass('flap-again');
+
 
 	});
